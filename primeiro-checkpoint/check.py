16cb2c024f3633b1300d1a8811a6e61050ef8a76
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2023-03-15 16:24:25 -0300

    linha reta ligando os centros dos círculos, ângulo, área e massa

--- primeiro-checkpoint/check.py
+++ primeiro-checkpoint/check.py
@@ -1,4 +1,5 @@
 #import das dependências
+import math
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
@@ -33,22 +34,43 @@ cv2.drawContours(contornos_img, contornos, -1, [255, 0, 0], 5)
 #tamanho e cor da cruz
 size = 20
 color = (115,251,253)
-count = 0
+
+coordenadasX = []
+coordenadasY = []
+
 for contorno in contornos:
-    cnt = contornos[count]
-    M = cv2.moments(cnt)
+    M = cv2.moments(contorno)
     #centro das formas
     cx = int(M['m10']/M['m00'])
     cy = int(M['m01']/M['m00'])
+    coordenadasX.append(cx)
+    coordenadasY.append(cy)
     #desenhar a cruz
     cv2.line(contornos_img,(cx - size,cy),(cx + size,cy),color,5)
     cv2.line(contornos_img,(cx,cy - size),(cx, cy + size),color,5)
     # Escrever a massa
     font = cv2.FONT_HERSHEY_SIMPLEX
     text = cy , cx
-    origem = (cx - 100,cy - 100)
-    cv2.putText(contornos_img, str(text), origem, font,1,(200,50,0),2,cv2.LINE_AA)
-    count += 1
+    origem_posicao = (cx - 105,cy - 90)
+    origem_area = (cx - 105,cy - 110)
+    area = cv2.contourArea(contorno)
+    cv2.putText(contornos_img, "posicao: "+str(text), origem_posicao, font,0.65,(200,50,0),1,cv2.LINE_AA)
+    cv2.putText(contornos_img, "area: "+str(area), origem_area, font,0.65,(200,50,0),1,cv2.LINE_AA)
+
+#calcular o ângulo de inclinação
+dx = coordenadasX[0] - coordenadasX[1]
+dy = coordenadasY[0] - coordenadasY[1]
+
+rad = math.atan2(dy, dx)
+angulo = math.degrees(rad)
+
+if angulo < 0:
+    angulo += 360
+cv2.putText(contornos_img, "angulo: "+str(angulo), (0, 500), font,0.6,(200,50,0),1,cv2.LINE_AA)
+
+
+color = (250,120,230)
+cv2.line(contornos_img,(coordenadasX[0],coordenadasY[0]),(coordenadasX[1], coordenadasY[1]),color,5)
 
 
 #mostra as imagens
