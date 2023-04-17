f01a7eec9342fdc10faf36c2a9d67bd615d787d5
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2023-04-16 21:41:35 -0300

    reconhecendo pedra no video através da técnica de features

--- segundo-checkpoint/jogo.py
+++ segundo-checkpoint/jogo.py
@@ -28,29 +28,44 @@ video = cv2.VideoCapture('pedra-papel-tesoura.mp4')
 #fgbg = cv2.createBackgroundSubtractorMOG2()
 #fgbg = cv2.createBackgroundSubtractorKNN()
 #
-#while(1):
-#    ret, frame = video.read()
-#    
-#    if not ret:
-#        break
-#    
-#    # Aplica a mascara no frame recebido
-#    #fgmask = fgbg.apply(frame)
-#    video_resized = cv2.resize(frame,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
-#    cv2.imshow('fgmask',video_resized)
-#    #cv2.imshow('frame',fgmask)
-#
-#    
-#    k = cv2.waitKey(30) & 0xff
-#    if k == 27:
-#        break
-#    
-#
-#video.release()
-#cv2.destroyAllWindows()
+while(1):
+    ret, frame = video.read()
+    
+    if not ret:
+        break
+    
+    # Aplica a mascara no frame recebido
+    #fgmask = fgbg.apply(frame)
+
+    # cria o objeto bf (Brute-force descriptor matcher)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    kp_frame, des_frame = orb.detectAndCompute(frame,None)
+
+    # a função match devolve os matches encontrados
+    matches = bf.match(des_pedra,des_frame)
+
+    print("Foram encontrados: {} matches".format(len(matches)))
+
+    img_matches = cv2.drawMatches(img_pedra,kp_pedra,frame,kp_frame,matches,None, flags=2)
+
+
+
+    video_resized = cv2.resize(img_matches,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+    cv2.imshow('fgmask',video_resized)
+    #cv2.imshow('frame',fgmask)
+
+    
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    
+
+video.release()
+cv2.destroyAllWindows()
 
 #cv2.imshow(pedra_kp_img)
 
 #mostra as imagens
-plt.imshow(pedra_kp_img)
-plt.show()
+#plt.imshow(pedra_kp_img)
+#plt.show()
